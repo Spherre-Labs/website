e3be8e464c58aed71483568df49cf926ac48c407
DanielEmmanuel1 <oluwadamilare_daniel@outlook.com>
2025-02-23 13:31:20 +0100

    structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Footer from "./Components/Footer";
 import Nav from "./Components/Nav";
 
 export default function Home() {
   return (
     <>
       <Nav />
+      <Footer/>
     </>
   );
 }
